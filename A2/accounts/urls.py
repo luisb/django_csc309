30f10f8fd1e7e2691f4b144de7e4e5f9fd10a9f5
Luis Baquera <luis.baquera@ucr.edu>
2024-07-31 15:37:44 -0700

    Small cleanup changes

--- A2/accounts/urls.py
+++ A2/accounts/urls.py
@@ -5,10 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    #path('login/', views.login, name='login'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.logout, name='logout'),
-    #path('register/', views.register, name='register'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('profile/view/', views.profile_view, name='profile-view'),
     path('profile/edit/', views.profile_edit, name='profile-edit'),
